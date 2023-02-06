a4a982e5352becfc3c533fc0b0dd83c534b0819d
jotoik <j.toikkanen@hotmail.com>
2023-02-06 12:04:29 +0200

    ga script

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,24 @@ export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>Joni Toikkanen Blogi</title>
+         {/* Global Site Tag (gtag.js) - Google Analytics */}
+         <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Tämä blogi on tehty Next.js:llä</p>
